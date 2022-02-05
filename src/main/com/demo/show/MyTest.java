bd946850cd1fe85e03a45580dc627d7840143522
andyluo882016 <andyluo882016@gmail.com>
2022-02-05 13:18:47 -0800

    Initial commit

---
+++ src/main/com/demo/show/MyTest.java
@@ -0,0 +1,28 @@
+public class MyTest {
+
+    String id;
+    String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public MyTest(String id, String name) {
+        this.id =id;
+        this.name=name;
+    }
+    public static void main(String[] args){
+        System.out.println("Test me");
+    }
+}
